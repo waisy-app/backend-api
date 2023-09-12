34d238c62ce54bc48671099fa3a3ced37df70505
mrkeksz <mrkeksz@yandex.ru>
2023-09-12 18:02:02 +0300

    devtools

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import {Module} from '@nestjs/common'
 import {AppController} from './app.controller'
 import {AppService} from './app.service'
+import {DevtoolsModule} from '@nestjs/devtools-integration'
+
+const isDev = process.env['NODE_ENV'] !== 'production'
 
 @Module({
-  imports: [],
+  imports: [
+    DevtoolsModule.register({
+      http: isDev,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import {NestFactory} from '@nestjs/core'
+import {NestFactory, PartialGraphHost} from '@nestjs/core'
 import {AppModule} from './app.module'
+import {writeFileSync} from 'fs'
+import {NestExpressApplication} from '@nestjs/platform-express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    snapshot: true,
+    abortOnError: false,
+  })
   await app.listen(3000)
 }
-void bootstrap()
+void bootstrap().catch(() => {
+  writeFileSync('graph.json', PartialGraphHost.toString() ?? '')
+  process.exit(1)
+})
