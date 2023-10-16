34b719897c52bcff2f48943c59b27d19c1e767ec
mrkeksz <mrkeksz@yandex.ru>
2023-10-16 16:06:09 +0700

    port env

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import {NestFactory, PartialGraphHost} from '@nestjs/core'
 import {AppModule} from './app.module'
 import {writeFileSync} from 'fs'
 import {NestExpressApplication} from '@nestjs/platform-express'
+import * as process from 'process'
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     snapshot: true,
     abortOnError: false,
   })
-  await app.listen(3000)
+  await app.listen(process.env['PORT'] ?? 3000)
 }
 void bootstrap().catch(() => {
   const isDev = process.env['NODE_ENV'] !== 'production'
