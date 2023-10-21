64828c090683aa99a5b73981fbab291007a85505
mrkeksz <mrkeksz@yandex.ru>
2023-10-21 17:02:18 +0700

    logging requests, responses and exceptions

--- src/filters/http-exception.filter.ts
+++ src/filters/http-exception.filter.ts
@@ -1,9 +1,18 @@
-import {ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus} from '@nestjs/common'
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+  HttpStatus,
+  Logger,
+} from '@nestjs/common'
 import {Request, Response} from 'express'
 import {EnvironmentConfigService} from '../config/environment/environment.config.service'
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
+  private readonly logger = new Logger(HttpExceptionFilter.name)
+
   constructor(private readonly environmentConfigService: EnvironmentConfigService) {}
 
   catch(exception: HttpException, host: ArgumentsHost) {
@@ -14,13 +23,11 @@ export class HttpExceptionFilter implements ExceptionFilter {
 
     response.status(status).json(exception.getResponse())
 
-    if (status < HttpStatus.INTERNAL_SERVER_ERROR && !this.environmentConfigService.isTest) {
-      console.error(
-        `[Response] [${
-          request.method
-        }] ${new Date().toLocaleDateString()}, ${new Date().toLocaleTimeString()} - Path ${
-          request.url
-        }, Status ${status}, Message: ${exception.cause || exception.message}`,
+    if (status < HttpStatus.INTERNAL_SERVER_ERROR && this.environmentConfigService.isDevelopment) {
+      const timeAgo = Date.now() - response.locals.startTime
+      this.logger.verbose(
+        `Response {${request.url}, ${request.method}, ${status}} \x1b[33m+${timeAgo}ms`,
+        `Body ${JSON.stringify(exception.getResponse())}`,
       )
     }
   }
