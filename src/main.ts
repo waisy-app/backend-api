c840f2e0578c1d9132755cfc19d439ca0a60b37e
mrkeksz <mrkeksz@yandex.ru>
2023-10-22 23:08:51 +0700

    debug logs

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,20 @@ import {AppModule} from './app.module'
 import {writeFileSync} from 'fs'
 import {NestExpressApplication} from '@nestjs/platform-express'
 import {ServerConfigService} from './config/server/server.config.service'
+import {NODE_ENV} from './config/environment/environment.config.constants'
 
 async function bootstrap() {
+  console.time('Nest')
+  console.log('Start')
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     snapshot: true,
     abortOnError: false,
   })
   const serverConfigService = app.get(ServerConfigService)
+  console.log('Listening on port', serverConfigService.port)
+  console.log(process.env[NODE_ENV.name])
   await app.listen(serverConfigService.port)
+  console.timeEnd('Nest')
 }
 void bootstrap().catch(() => {
   writeFileSync('error-graph.json', PartialGraphHost.toString() ?? '')
