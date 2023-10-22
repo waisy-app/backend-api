92e390606c3ce80e2195ae79462571661235e60c
mrkeksz <mrkeksz@yandex.ru>
2023-10-23 02:24:51 +0700

    error-graph

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,19 @@ import {AppModule} from './app.module'
 import {writeFileSync} from 'fs'
 import {NestExpressApplication} from '@nestjs/platform-express'
 import {ServerConfigService} from './config/server/server.config.service'
+import {NODE_ENV} from './config/environment/environment.config.constants'
+
+const isDev = process.env[NODE_ENV.name] === NODE_ENV.options.DEVELOPMENT
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    snapshot: true,
-    abortOnError: false,
+    snapshot: isDev,
+    abortOnError: !isDev,
   })
   const serverConfigService = app.get(ServerConfigService)
   await app.listen(serverConfigService.port)
 }
 void bootstrap().catch(() => {
-  writeFileSync('error-graph.json', PartialGraphHost.toString() ?? '')
+  if (isDev) writeFileSync('error-graph.json', PartialGraphHost.toString() ?? '')
   process.exit(1)
 })
