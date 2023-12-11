22c5a62daabc8a74bc71059fc86fab68adb72eb2
mrkeksz <mrkeksz@yandex.ru>
2023-12-11 19:30:44 +0700

    Refactor email verification resolver and tests

--- src/email-verification/email-verification.resolver.spec.ts
+++ src/email-verification/email-verification.resolver.spec.ts
@@ -1,42 +1,74 @@
 import {Test, TestingModule} from '@nestjs/testing'
-import {EmailVerificationService} from './email-verification.service'
 import {EmailVerificationResolver} from './email-verification.resolver'
+import {EmailVerificationService} from './email-verification.service'
+import {EmailVerificationSendingLimitService} from './email-verification-sending-limit.service'
+import {EmailVerificationInputLimitService} from './email-verification-input-limit.service'
+import {SendVerificationCodeToEmailArgs} from './dto/send-verification-code-to-email.args'
+import {ForbiddenException} from '@nestjs/common'
+import {VerifyEmailCodeArgs} from './dto/verify-email-code.args'
 
 describe('EmailVerificationResolver', () => {
   let resolver: EmailVerificationResolver
-  let service: EmailVerificationService
+  let mockEmailVerificationService: any
+  let mockSendingLimitService: any
+  let mockInputLimitService: any
 
   beforeEach(async () => {
+    mockEmailVerificationService = {sendVerificationCodeToEmail: jest.fn(), verifyEmail: jest.fn()}
+    mockSendingLimitService = {enforceEmailVerificationSendingLimit: jest.fn()}
+    mockInputLimitService = {
+      enforceEmailVerificationInputLimit: jest.fn(),
+      createInputAttempt: jest.fn(),
+    }
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         EmailVerificationResolver,
-        {
-          provide: EmailVerificationService,
-          useValue: {
-            enforceEmailVerificationSendingLimit: jest.fn(),
-            sendVerificationCodeToEmail: jest.fn(),
-          },
-        },
+        {provide: EmailVerificationService, useValue: mockEmailVerificationService},
+        {provide: EmailVerificationSendingLimitService, useValue: mockSendingLimitService},
+        {provide: EmailVerificationInputLimitService, useValue: mockInputLimitService},
       ],
     }).compile()
 
-    resolver = module.get(EmailVerificationResolver)
-    service = module.get(EmailVerificationService)
+    resolver = module.get<EmailVerificationResolver>(EmailVerificationResolver)
   })
 
-  afterEach(() => {
-    jest.resetAllMocks()
+  it('should send verification code successfully', async () => {
+    const args = new SendVerificationCodeToEmailArgs()
+    args.email = 'test@example.com'
+    await resolver.sendVerificationCodeToEmail(args, '127.0.0.1')
+    expect(mockSendingLimitService.enforceEmailVerificationSendingLimit).toHaveBeenCalled()
+    expect(mockEmailVerificationService.sendVerificationCodeToEmail).toHaveBeenCalled()
   })
 
-  it('should send verification code to an email', async () => {
-    const email = 'test@test.com'
-    const clientIp = '127.0.0.1'
+  it('should throw error when send email limit exceeded', async () => {
+    const args = new SendVerificationCodeToEmailArgs()
+    args.email = 'test@example.com'
+    mockSendingLimitService.enforceEmailVerificationSendingLimit.mockImplementation(() => {
+      throw new ForbiddenException()
+    })
+    await expect(resolver.sendVerificationCodeToEmail(args, '127.0.0.1')).rejects.toThrow(
+      ForbiddenException,
+    )
+  })
 
-    jest
-      .spyOn(service, 'enforceEmailVerificationSendingLimit')
-      .mockImplementation(() => Promise.resolve())
-    jest.spyOn(service, 'sendVerificationCodeToEmail').mockImplementation(() => Promise.resolve())
+  it('should verify email code successfully', async () => {
+    const args = new VerifyEmailCodeArgs()
+    args.email = 'test@example.com'
+    args.code = 123456
+    await resolver.verifyEmailCode('127.0.0.1', args)
+    expect(mockInputLimitService.enforceEmailVerificationInputLimit).toHaveBeenCalled()
+    expect(mockEmailVerificationService.verifyEmail).toHaveBeenCalled()
+    expect(mockInputLimitService.createInputAttempt).toHaveBeenCalled()
+  })
 
-    await expect(resolver.sendVerificationCodeToEmail({email}, clientIp)).resolves.toBeTruthy()
+  it('should throw error when verify email code failed', async () => {
+    const args = new VerifyEmailCodeArgs()
+    args.email = 'test@example.com'
+    args.code = 123456
+    mockEmailVerificationService.verifyEmail.mockImplementation(() => {
+      throw new ForbiddenException()
+    })
+    await expect(resolver.verifyEmailCode('127.0.0.1', args)).rejects.toThrow(ForbiddenException)
   })
 })
--- src/email-verification/email-verification.resolver.ts
+++ src/email-verification/email-verification.resolver.ts
@@ -1,15 +1,20 @@
 import {Args, Mutation, Resolver} from '@nestjs/graphql'
 import {SendVerificationCodeToEmailArgs} from './dto/send-verification-code-to-email.args'
-import {Logger} from '@nestjs/common'
 import {ClientIp} from '../utils/client-ip.decorator'
 import {SkipJwtAuth} from '../auth/decorators/skip-jwt-auth.decorator'
 import {EmailVerificationService} from './email-verification.service'
+import {EmailVerificationSendingLimitService} from './email-verification-sending-limit.service'
+import {EmailVerificationInputLimitService} from './email-verification-input-limit.service'
+import {VerifyEmailCodeArgs} from './dto/verify-email-code.args'
+import {Auth} from '../auth/models/auth.model'
 
 @Resolver()
 export class EmailVerificationResolver {
-  private readonly logger = new Logger(EmailVerificationResolver.name)
-
-  constructor(private readonly emailVerificationService: EmailVerificationService) {}
+  constructor(
+    private readonly emailVerificationService: EmailVerificationService,
+    private readonly sendingLimitService: EmailVerificationSendingLimitService,
+    private readonly inputLimitService: EmailVerificationInputLimitService,
+  ) {}
 
   @SkipJwtAuth()
   @Mutation(() => Boolean, {description: 'Send verification code to email'})
@@ -17,11 +22,25 @@ export class EmailVerificationResolver {
     @Args() {email}: SendVerificationCodeToEmailArgs,
     @ClientIp() clientIp: string,
   ): Promise<true> {
-    this.logger.log(`sendVerificationCodeToEmail: ${email} from ${clientIp}`)
-    await this.emailVerificationService.enforceEmailVerificationSendingLimit(clientIp, email)
+    await this.sendingLimitService.enforceEmailVerificationSendingLimit(clientIp, email)
     await this.emailVerificationService.sendVerificationCodeToEmail(email)
     return true
   }
 
-  // TODO: метод проверки введенного кода. Метод возращает токены, которые можно использовать для авторизации
+  @SkipJwtAuth()
+  @Mutation(() => Auth, {description: 'Verify email code'})
+  public async verifyEmailCode(
+    @ClientIp() senderIp: string,
+    @Args() {email, code}: VerifyEmailCodeArgs,
+  ): Promise<Auth> {
+    await this.inputLimitService.enforceEmailVerificationInputLimit(senderIp)
+    try {
+      const result = this.emailVerificationService.verifyEmail(email, code)
+      await this.inputLimitService.createInputAttempt({senderIp, email, status: 'success'})
+      return result
+    } catch (error: unknown) {
+      await this.inputLimitService.createInputAttempt({senderIp, email, status: 'failure'})
+      throw error
+    }
+  }
 }
