35f1b3499ad8a050cce023dea42955c9b32d7520
mrkeksz <mrkeksz@yandex.ru>
2023-12-25 07:36:10 +0300

    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,64 +1,35 @@
-import {MiddlewareConsumer, Module, NestModule, RequestMethod, ValidationPipe} from '@nestjs/common'
+import {Module} from '@nestjs/common'
 import {UsersModule} from './users/users.module'
-import {RequestLoggerMiddleware} from './middlewares/request-logger.middleware'
-import {APP_FILTER, APP_INTERCEPTOR, APP_PIPE} from '@nestjs/core'
-import {HttpExceptionFilter} from './exception-filters/http-exception.filter'
-import {LoggingInterceptor} from './interceptors/logging.interceptor'
-import {TimeoutInterceptor} from './interceptors/timeout.interceptor'
 import {GraphQLModule} from '@nestjs/graphql'
-import {ComplexityPlugin} from './apollo-plugins/complexity.plugin'
-import {AllExceptionsFilter} from './exception-filters/all-exception.filter'
-import {GraphqlExceptionFilter} from './exception-filters/graphql-exception.filter'
-import {ErrorFormatterModule} from './error-formatter/error-formatter.module'
+import {GraphqlComplexityPlugin} from './graphql/graphql-complexity.plugin'
 import {TypeOrmModule} from '@nestjs/typeorm'
-import {CryptService} from './crypt/crypt.service'
 import {typeOrmModuleOptions} from './type-orm-module.options'
-import {graphqlModuleOptions} from './graphql-module.options'
+import {graphqlModuleOptions} from './graphql/graphql-module.options'
 import {ConfigModule} from './config/config.module'
 import {EmailVerificationModule} from './email-verification/email-verification.module'
 import {RefreshTokenModule} from './refresh-token/refresh-token.module'
-
+import {ErrorsModule} from './errors/errors.module'
+import {LoggerModule} from './logger/logger.module'
+import {RequestTimeoutModule} from './request-timeout/request-timeout.module'
+import {ValidationModule} from './validation/validation.module'
+import {GraphqlModule} from './graphql/graphql.module'
+import {CryptModule} from './crypt/crypt.module'
+// TODO: переписать тесты под нативный test_runner
 @Module({
   imports: [
-    UsersModule,
-    ErrorFormatterModule,
-    ConfigModule,
     TypeOrmModule.forRootAsync(typeOrmModuleOptions),
     GraphQLModule.forRootAsync(graphqlModuleOptions),
+    UsersModule,
+    ConfigModule,
     EmailVerificationModule,
     RefreshTokenModule,
+    ErrorsModule,
+    LoggerModule,
+    RequestTimeoutModule,
+    ValidationModule,
+    GraphqlModule,
+    CryptModule,
   ],
-  providers: [
-    ComplexityPlugin,
-    CryptService,
-    {
-      provide: APP_FILTER,
-      useClass: AllExceptionsFilter,
-    },
-    {
-      provide: APP_FILTER,
-      useClass: GraphqlExceptionFilter,
-    },
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: TimeoutInterceptor,
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: LoggingInterceptor,
-    },
-    {
-      provide: APP_PIPE,
-      useClass: ValidationPipe,
-    },
-  ],
+  providers: [GraphqlComplexityPlugin],
 })
-export class AppModule implements NestModule {
-  public configure(consumer: MiddlewareConsumer): void {
-    consumer.apply(RequestLoggerMiddleware).forRoutes({path: '*', method: RequestMethod.ALL})
-  }
-}
+export class AppModule {}
